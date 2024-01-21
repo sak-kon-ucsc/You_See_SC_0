f81be812c3db2eda6b39ebfae1c4b296325ce4dc
sak-kon-ucsc <108996976+sak-kon-ucsc@users.noreply.github.com>
2024-01-20 16:29:52 -0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "You_See_SC_0"
+include(":app")
+ 
